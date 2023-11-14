b7bf139869606bb95b83a2f7b5536b66372d9b85
Michael Mersiades <fakename014@gmail.com>
2023-11-14 17:53:15 +1100

    Adds form

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,14 +1,26 @@
 import { StyleSheet } from 'react-native';
 
-import EditScreenInfo from '../../components/EditScreenInfo';
-import { Text, View } from '../../components/Themed';
+import { View } from '../../components/Themed';
+import {Controller, useForm, useWatch} from "react-hook-form";
+
+type FormValues = {
+  email: string;
+};
 
 export default function TabOneScreen() {
+  const {
+    control,
+  } = useForm<FormValues>();
+
+  const emailWatch = useWatch({
+    control,
+    name: 'email',
+  });
+
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Tab One</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="app/(tabs)/index.tsx" />
+      <Controller control={control} name={'email'} render={() => <View/>} />
+      <Controller control={control} name={'password'} render={() => <View/>} />
     </View>
   );
 }
@@ -18,14 +30,5 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
-  },
-  title: {
-    fontSize: 20,
-    fontWeight: 'bold',
-  },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: '80%',
   },
 });
