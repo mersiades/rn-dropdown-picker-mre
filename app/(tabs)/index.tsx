9f40354d6ae68a38fc5bd42e59624b03f36d1515
Michael Mersiades <fakename014@gmail.com>
2024-01-18 18:26:49 +1100

    Adds dropdown picker example

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,26 +1,42 @@
 import { StyleSheet } from 'react-native';
+import DropDownPicker, {
+} from 'react-native-dropdown-picker';
 
 import { View } from '../../components/Themed';
-import {Controller, useForm, useWatch} from "react-hook-form";
-
-type FormValues = {
-  email: string;
-};
+import {useState} from "react";
 
 export default function TabOneScreen() {
-  const {
-    control,
-  } = useForm<FormValues>();
+  const defaultItems = [
+    { label: 'Australia', value: 'au' },
+    { label: 'Indonesia', value: 'id' },
+    { label: 'New Zealand', value: 'nz' },
+  ];
 
-  const emailWatch = useWatch({
-    control,
-    name: 'email',
-  });
+  const [open, setOpen] = useState(false);
+  const [value, setValue] = useState<string[]>([]);
+  const [items, setItems] = useState(defaultItems);
 
   return (
     <View style={styles.container}>
-      <Controller control={control} name={'email'} render={() => <View/>} />
-      <Controller control={control} name={'password'} render={() => <View/>} />
+      <DropDownPicker
+        schema={{
+          label: 'label',
+          value: 'value',
+        }}
+        mode="BADGE"
+        listMode={'MODAL'}
+        value={value}
+        items={items}
+        setItems={setItems}
+        open={open}
+        setOpen={setOpen}
+        setValue={(cb) => {
+          const newValue = cb(value);
+          setValue(newValue);
+        }}
+        multiple
+        searchable
+      />
     </View>
   );
 }
